b7364f39a3cf5f5923e4f31e92bf1d8030f5bee0
karlaortiz <karlaortiz@berkeley.edu>
2018-07-16 03:17:19 -0400

    finished custom interaction

--- draw/urls.py
+++ draw/urls.py
@@ -1,10 +1,16 @@
 # chat/urls.py
 from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+#     url(r'^$', views.customInteraction, name='customInteraction'),
+    path('smallInteraction1/', views.smallInteraction1, name='smallInteraction1'),
+    path('smallInteraction2/', views.smallInteraction2, name='smallInteraction2'),
+    path('largeDisplay/', views.largeDisplay, name='largeDisplay'),
+    path('teacherDisplay/', views.teacherDisplay, name='teacherDisplay'),
     url(r'^(?P<room_name>[^/]+)/$', views.room, name='room'),
 ]
 
--- draw/views.py
+++ draw/views.py
@@ -4,7 +4,19 @@ import json
 
 def index(request):
     return render(request, 'draw/index.html', {})
+  
+def smallInteraction1(request):
+    return render(request, 'draw/smallInteraction1.html', {})
 
+def smallInteraction2(request):
+    return render(request, 'draw/smallInteraction2.html', {})
+
+def largeDisplay(request):
+    return render(request, 'draw/largeDisplay.html', {})
+
+def teacherDisplay(request):
+    return render(request, 'draw/teacherDisplay.html', {})
+  
 def room(request, room_name):
     return render(request, 'draw/room.html', {
         'room_name_json': mark_safe(json.dumps(room_name))
